038511bb545336ce2fbca1580e4f29e7a9ae4ed7
Emilien <emiliengir@gmail.com>
2024-01-26 10:37:41 +0100

    test"

--- whatyearisit-frontend/script.js
+++ whatyearisit-frontend/script.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/year')
+fetch('https://year-93dx572va-emjy-67ba5b66.vercel.app/year')
     .then(response => response.json())
     .then((data) => {
         document.querySelector('#year').textContent = data.year;
